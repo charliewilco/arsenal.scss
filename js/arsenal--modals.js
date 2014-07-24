12f2553174075b62145f246ed319ff674c99ed93
charlespeters <charlespeters42@gmail.com>
2014-07-24 04:42:40 -0400

    working through modals.

--- js/arsenal--modals.js
+++ js/arsenal--modals.js
@@ -1,33 +1,3 @@
-var modal = (function(){
-    var
-    method = {},
-    $overlay,
-    $modal,
-    $content,
-    $close;
-
-    // Append the HTML
-
-    // Center the modal in the viewport
-    method.center = function () {};
-
-    // Open the modal
-    method.open = function (settings) {};
-
-    // Close the modal
-    method.close = function () {};
-
-    return method;
-}());
-$overlay = $('<div id="overlay"></div>');
-$modal = $('<div id="modal"></div>');
-$content = $('<div id="content"></div>');
-$close = $('<a id="close" href="#">close</a>');
-
-$modal.hide();
-$overlay.hide();
-$modal.append($content, $close);
-
-$(document).ready(function(){
-    $('body').append($overlay, $modal);
-});
+function arsenalModal {
+    console.log('dance with modals');
+}
