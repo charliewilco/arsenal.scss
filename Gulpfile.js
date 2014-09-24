e54bcd52a1d94a3f9d03b8c5ba46a30711ee3df4
charlespeters <charlespeters42@gmail.com>
2014-09-23 23:41:44 -0400

    updating the gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -19,7 +19,7 @@ gulp.task('scripts', function(){
 gulp.task('styles', function(){
     gulp.src('scss/**/*.scss')
         .pipe(plumber())
-        .pipe(sass({style: 'compressed', sourcemap: true}))
+        .pipe(sass({style: 'compressed'}))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(gulp.dest('build/css/'))
         .pipe(connect.reload());
@@ -56,4 +56,6 @@ gulp.task('images', function () {
         .pipe(gulp.dest('build/img/'));
 });
 
+
 gulp.task('default', ['html','scripts', 'styles', 'images', 'watch', 'connect']);
+gulp.task('build', ['html','scripts', 'styles', 'images']);
