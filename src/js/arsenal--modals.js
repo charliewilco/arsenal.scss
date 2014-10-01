0e2b0099274432dbb20a08a1544364798042c945
charlespeters <charlespeters42@gmail.com>
2014-09-30 20:47:37 -0400

    updates

--- src/js/arsenal--modals.js
+++ src/js/arsenal--modals.js
@@ -1,3 +1,16 @@
-function arsenalModal() {
-    console.log('dance with modals');
-}
+var modal = $('.modal');
+
+$( "#modal--open" ).on( "click", function() {
+  $( modal ).toggleClass('modal--show');
+});
+
+$( ".overlay" ).on( "click", function() {
+  $( modal ).toggleClass('modal--show');
+});
+
+$( ".modal--close" ).on( "click", function() {
+  $( modal ).toggleClass('modal--show');
+});
+
+
+// Forked from http://codepen.io/clintonhalpin/pen/cgbmk
