719f1ca34974425c44783784b05e9633834c7224
charlespeters <charlespeters42@gmail.com>
2014-10-10 04:59:52 -0400

    making so many changes

--- Gulpfile.js
+++ Gulpfile.js
@@ -20,7 +20,7 @@ gulp.task('scripts', function(){
 gulp.task('styles', function(){
     gulp.src('src/scss/**/*.scss')
         .pipe(plumber())
-        .pipe(sass({style: 'compressed'}))
+        .pipe(sass({style: 'expanded'}))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(gulp.dest('build/css/'))
         .pipe(connect.reload());
@@ -64,3 +64,5 @@ gulp.task('images', function () {
 
 gulp.task('default', ['fileinclude','scripts', 'styles', 'images', 'watch', 'connect']);
 gulp.task('build', ['fileinclude','scripts', 'styles', 'images']);
+
+module.exports = gulp;
