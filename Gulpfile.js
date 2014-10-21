b7456ff50ee0dfeaf78971c03692ba0437441ca9
charlespeters <charlespeters42@gmail.com>
2014-10-21 00:49:22 -0400

    lots of updates

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,7 @@
 var gulp         = require('gulp'),
     uglify       = require('gulp-uglify'),
     sass         = require('gulp-ruby-sass'),
+    sourcemaps   = require('gulp-sourcemaps'),
     plumber      = require('gulp-plumber'),
     autoprefixer = require('gulp-autoprefixer'),
     connect      = require('gulp-connect'),
@@ -20,8 +21,10 @@ gulp.task('scripts', function(){
 gulp.task('styles', function(){
     gulp.src('src/scss/**/*.scss')
         .pipe(plumber())
-        .pipe(sass({style: 'expanded'}))
+        .pipe(sourcemaps.init())
+        .pipe(sass({style: 'condensed'}))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('build/css/'))
         .pipe(connect.reload());
 });
